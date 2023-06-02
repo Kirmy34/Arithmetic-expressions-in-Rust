34849d91eed7ac7f3a45b33eb78f0fcc4e3cfad8
Jonas Heck <heckjonas10.05.00@gmail.com>
2023-06-02 19:05:24 +0200

    Feature: Hinzufügen des Datentyps Expression als ein Enum
    Außerdem hinzufügen, von zwei einfachen Test Expressions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use crate::Expression::{Mult, One, Plus, Zero};
+
+enum Expression {
+    One,
+    Zero,
+    ETrue,
+    EFalse,
+    Plus(Box<Expression>, Box<Expression>),
+    Mult(Box<Expression>, Box<Expression>),
+    Or(Box<Expression>, Box<Expression>),
+}
+
 fn main() {
+    let expression1 = Plus(Box::new(Mult(Box::new(One), Box::new(Zero))), Box::new(One));
+    let dxpression2 = Mult(Box::new(Zero), Box::new(Plus(Box::new(Zero), Box::new(One))));
+
     println!("Hello, world!");
 }

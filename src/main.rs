be388eade5fb72ecc2f278495b128422eab1cfe0
Jonas Heck <heckjonas10.05.00@gmail.com>
2023-06-02 19:59:34 +0200

    Feature: Hinzufügen der Show Methode, sowie einer And Expression
    ich fand, dass Logik Expression mit nur Or langweilig sind, deswegen habe ich noch eine And Expression hinzugefügt, außerdem habe ich die show Methode implementiert, sowie 5 einfache Test Expressions, die für show alle funktionieren, für eval und typecheck werden bzw. sollten die kaputt gehen, zumindest manche davon.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::Expression::{Mult, One, Plus, Zero};
+use crate::Expression::{EAnd, EFalse, EOr, ETrue, Mult, One, Plus, Zero};
 
 enum Expression {
     One,
@@ -7,12 +7,41 @@ enum Expression {
     EFalse,
     Plus(Box<Expression>, Box<Expression>),
     Mult(Box<Expression>, Box<Expression>),
-    Or(Box<Expression>, Box<Expression>),
+    EOr(Box<Expression>, Box<Expression>),
+    EAnd(Box<Expression>, Box<Expression>),
+}
+
+impl Expression {
+    fn show(&self) -> String {
+        return match self {
+            Zero => String::from("0"),
+            One => String::from("1"),
+            ETrue => String::from("true"),
+            EFalse => String::from("false"),
+            Plus(left, right) =>
+                format!("({} + {})", left.show(), right.show()),
+            Mult(left, right) =>
+                format!("({} * {})", left.show(), right.show()),
+            EOr(left, right) =>
+                format!("({} || {})", left.show(), right.show()),
+            EAnd(left, right) =>
+                format!("({} || {})", left.show(), right.show()),
+            _ => String::from("failure") // should never happen, just in case.
+        };
+    }
 }
 
 fn main() {
     let expression1 = Plus(Box::new(Mult(Box::new(One), Box::new(Zero))), Box::new(One));
-    let dxpression2 = Mult(Box::new(Zero), Box::new(Plus(Box::new(Zero), Box::new(One))));
+    let expression2 = Mult(Box::new(Zero), Box::new(Plus(Box::new(Zero), Box::new(One))));
+    let expression3 = EOr(Box::new(ETrue), Box::new(EAnd(Box::new(ETrue), Box::new(EFalse))));
+    let expression4 = Mult(Box::new(Zero), Box::new(EOr(Box::new(EFalse), Box::new(ETrue))));
+    let expression5 = Mult(Box::new(Zero), Box::new(EOr(Box::new(Zero), Box::new(One))));
 
     println!("Hello, world!");
+    println!("{}", expression1.show());
+    println!("{}", expression2.show());
+    println!("{}", expression3.show());
+    println!("{}", expression4.show());
+    println!("{}", expression5.show());
 }

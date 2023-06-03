9e0c6a0484d853d01357ef7aa374eaac6c8525f4
Jonas Heck <heckjonas10.05.00@gmail.com>
2023-06-03 10:12:49 +0200

    Feature: Weniger Klammern

--- src/expression_show.rs
+++ src/expression_show.rs
@@ -15,10 +15,37 @@ impl Expression {
             Self::Nine => String::from("9"),
             Self::ETrue => String::from("true"),
             Self::EFalse => String::from("false"),
-            Self::Plus(left, right) => format!("({} + {})", left.show(), right.show()),
-            Self::Mult(left, right) => format!("{} * {}", left.show(), right.show()),
-            Self::EOr(left, right) => format!("({} || {})", left.show(), right.show()),
-            Self::EAnd(left, right) => format!("({} && {})", left.show(), right.show()),
+            Self::Plus(left, right) => format!("{} + {}", left.show_for_brackets(false), right.show_for_brackets(false)),
+            Self::Mult(left, right) => format!("{} * {}", left.show_for_brackets(true), right.show_for_brackets(true)),
+            Self::EOr(left, right) => format!("({} || {})", left.show_for_brackets(false), right.show_for_brackets(false)),
+            Self::EAnd(left, right) => format!("({} && {})", left.show_for_brackets(false), right.show_for_brackets(false)),
+        }
+    }
+
+    fn show_for_brackets(&self, parent_is_multiplication: bool) -> String {
+        match self {
+            Self::Zero => String::from("0"),
+            Self::One => String::from("1"),
+            Self::Two => String::from("2"),
+            Self::Three => String::from("3"),
+            Self::Four => String::from("4"),
+            Self::Five => String::from("5"),
+            Self::Six => String::from("6"),
+            Self::Seven => String::from("7"),
+            Self::Eight => String::from("8"),
+            Self::Nine => String::from("9"),
+            Self::ETrue => String::from("true"),
+            Self::EFalse => String::from("false"),
+            Self::Plus(left, right) => {
+                match parent_is_multiplication {
+                    false => format!("{} + {}", left.show_for_brackets(false), right.show_for_brackets(false)),
+                    true => format!("({} + {})", left.show_for_brackets(false), right.show_for_brackets(false)),
+                }
+
+            },
+            Self::Mult(left, right) => format!("{} * {}", left.show_for_brackets(true), right.show_for_brackets(true)),
+            Self::EOr(left, right) => format!("({} || {})", left.show_for_brackets(false), right.show_for_brackets(false)),
+            Self::EAnd(left, right) => format!("({} && {})", left.show_for_brackets(false), right.show_for_brackets(false)),
         }
     }
 }
@@ -29,7 +56,7 @@ mod test {
 
     #[test]
     fn test_show_without_brackets() {
-        assert_eq!(EXPRESSION1.show(), "(1 * 0 + 1)");
+        assert_eq!(EXPRESSION1.show(), "1 * 0 + 1");
     }
 
     #[test]
@@ -65,6 +92,6 @@ mod test {
 
     #[test]
     fn test_show_with_more_numbers() {
-        assert_eq!(EXPRESSION8.show(), "(3 * (6 + 7) + 8)");
+        assert_eq!(EXPRESSION8.show(), "3 * (6 + 7) + 8");
     }
 }
